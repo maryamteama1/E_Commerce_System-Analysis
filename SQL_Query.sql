7ffabf5f526b48f80f851449ea6caf72c86cd2fc
Maryam Taeima <maryamteama1@gmail.com>
2026-07-17 20:48:15 +0300

    Add files via upload

---
+++ SQL_Query.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE E_Commerce_System
+
+USE E_Commerce_System
+
+SELECT * FROM Categories
+SELECT * FROM [Customers (2)]
+SELECT * FROM Order_Details
+SELECT * FROM [Orders (2)]
+SELECT * FROM Payments
+SELECT * FROM [Products (2)]
+SELECT * FROM shipments
+SELECT * FROM Suppliers
+
+
+ALTER AUTHORIZATION ON DATABASE::E_Commerce_System TO sa;
+
